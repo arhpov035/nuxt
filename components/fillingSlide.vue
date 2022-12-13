25701dc22f74c3de8914d0de3cdbbb048fa64671
arhipov035 <arhipov035@gmail.com>
2022-12-13 23:19:46 +0300

    slick pavel

--- components/fillingSlide.vue
+++ components/fillingSlide.vue
@@ -1,52 +1,122 @@
 <template>
-  <div class="filling slide">
-    <div class="item">
-      <img src="/images/filling/nachinka-muss-chernaya-smorodina.jpg" alt="" />
-    </div>
-    <div class="item">
-      <img src="/images/filling/nachinka-napoleon.jpg" alt="" />
-    </div>
-    <div class="item">
-      <img src="/images/filling/nachinka-pancho.jpg" alt="" />
-    </div>
-    <div class="item">
-      <img src="/images/filling/nachinka-tri-shokolada.jpg" alt="" />
-    </div>
-  </div>
+  <div class="page_filling">
+  <slick
+    ref="slick"
+    :options="slickOptions"
+    @afterChange="handleAfterChange"
+    @beforeChange="handleBeforeChange"
+    @breakpoint="handleBreakpoint"
+    @destroy="handleDestroy"
+    @edge="handleEdge"
+    @init="handleInit"
+    @reInit="handleReInit"
+    @setPosition="handleSetPosition"
+    @swipe="handleSwipe"
+    @lazyLoaded="handleLazyLoaded"
+    @lazyLoadError="handleLazyLoadError"
+  >
+    <a href="https://avatars.mds.yandex.net/i?id=d8fcb3ed41532eb5dd6726df9cb1314b-5236332-images-thumbs&n=13"
+      ><img src="https://avatars.mds.yandex.net/i?id=d8fcb3ed41532eb5dd6726df9cb1314b-5236332-images-thumbs&n=13" alt=""
+    /></a>
+    <a href="https://avatars.mds.yandex.net/i?id=d8fcb3ed41532eb5dd6726df9cb1314b-5236332-images-thumbs&n=13"
+      ><img src="https://avatars.mds.yandex.net/i?id=d8fcb3ed41532eb5dd6726df9cb1314b-5236332-images-thumbs&n=13" alt=""
+    /></a>
+    <a href="https://avatars.mds.yandex.net/i?id=d8fcb3ed41532eb5dd6726df9cb1314b-5236332-images-thumbs&n=13"
+      ><img src="https://avatars.mds.yandex.net/i?id=d8fcb3ed41532eb5dd6726df9cb1314b-5236332-images-thumbs&n=13" alt=""
+    /></a>
+    <a href="https://avatars.mds.yandex.net/i?id=d8fcb3ed41532eb5dd6726df9cb1314b-5236332-images-thumbs&n=13"
+      ><img src="https://avatars.mds.yandex.net/i?id=d8fcb3ed41532eb5dd6726df9cb1314b-5236332-images-thumbs&n=13" alt=""
+    /></a>
+    <a href="https://avatars.mds.yandex.net/i?id=d8fcb3ed41532eb5dd6726df9cb1314b-5236332-images-thumbs&n=13"
+      ><img src="https://avatars.mds.yandex.net/i?id=d8fcb3ed41532eb5dd6726df9cb1314b-5236332-images-thumbs&n=13" alt=""
+    /></a>
+  </slick>
+</div>
 </template>
 
 <script>
+import Slick from "vue-slick";
 export default {
-  head() {
+  components: { Slick },
+
+  data() {
     return {
-      script: [
-        {
-          hid: "stripe",
-          src: "https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js",
-          defer: true,
-        },
-        { hid: "stripe", src: "/slick/slick.js", defer: true },
-        { hid: "stripe", src: "/js/script.js", defer: true },
-      ],
+      slickOptions: {
+        slidesToShow: 3,
+        // Any other options that can be got from plugin documentation
+      },
     };
+  },
+
+  // All slick methods can be used too, example here
+  methods: {
+    next() {
+      this.$refs.slick.next();
+    },
+
+    prev() {
+      this.$refs.slick.prev();
+    },
+
+    reInit() {
+      // Helpful if you have to deal with v-for to update dynamic lists
+      this.$nextTick(() => {
+        this.$refs.slick.reSlick();
+      });
+    },
+
+    // Events listeners
+    handleAfterChange(event, slick, currentSlide) {
+      console.log("handleAfterChange", event, slick, currentSlide);
+    },
+    handleBeforeChange(event, slick, currentSlide, nextSlide) {
+      console.log("handleBeforeChange", event, slick, currentSlide, nextSlide);
+    },
+    handleBreakpoint(event, slick, breakpoint) {
+      console.log("handleBreakpoint", event, slick, breakpoint);
+    },
+    handleDestroy(event, slick) {
+      console.log("handleDestroy", event, slick);
+    },
+    handleEdge(event, slick, direction) {
+      console.log("handleEdge", event, slick, direction);
+    },
+    handleInit(event, slick) {
+      console.log("handleInit", event, slick);
+    },
+    handleReInit(event, slick) {
+      console.log("handleReInit", event, slick);
+    },
+    handleSetPosition(event, slick) {
+      console.log("handleSetPosition", event, slick);
+    },
+    handleSwipe(event, slick, direction) {
+      console.log("handleSwipe", event, slick, direction);
+    },
+    handleLazyLoaded(event, slick, image, imageSource) {
+      console.log("handleLazyLoaded", event, slick, image, imageSource);
+    },
+    handleLazyLoadError(event, slick, image, imageSource) {
+      console.log("handleLazeLoadError", event, slick, image, imageSource);
+    },
   },
 };
 </script>
 
 <style scoped>
-.filling {
+.page_filling {
   max-width: 724px;
   width: 100%;
 }
 .slick-slide {
-   margin: 0 26px;
+  margin: 0 26px;
 }
 
 /* Родительский элемент */
 .slick-list {
-   margin: 0 -26px;
+  margin: 0 -26px;
 }
-img{
+img {
   width: 100%;
 }
 .item {
